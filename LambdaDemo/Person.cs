d3f1059b316f355a9f525e746160dc0224a234a2
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-10-19 09:11:48 +0530

    [Tirupathi] Add. Person Data using constructors

---
+++ LambdaDemo/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaDemo
+{
+
+    class Person
+    {
+        private int SSN;
+        private string name;
+        private string address;
+        private int age;
+
+        public Person(int SSN,string name,string address,int age)
+        {
+            this.SSN = SSN;
+            this.name = name;
+            this.address = address;
+            this.age = age;
+        }
+
+    }
+}
--- LambdaDemo/Program.cs
+++ LambdaDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LambdaDemo
 {
@@ -7,6 +8,26 @@ namespace LambdaDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to C# Lambda Expressions!");
+
+            //UC 1: Creating list and insert records
+            List<Person> personList = new List<Person>();
+            AddRecords(personList);
+
+        }
+
+        private static void AddRecords(List<Person> personList)
+        {
+            Person person1 = new Person(100, "ktr", "palasa", 22);
+            personList.Add(person1);
+
+            Person person2 = new Person(101, "tkr", "palas", 21);
+            personList.Add(person2);
+
+            Person person3 = new Person(102, "krt", "plasa", 20);
+            personList.Add(person3);
+
+            Person person4 = new Person(103, "rkt", "pasa", 19);
+            personList.Add(person4);
         }
     }
 }
