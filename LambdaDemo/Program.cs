91767e0c05a753a86930642b9a88d1f0b607cc04
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-10-19 09:42:23 +0530

    [Tirupathi] Add. Persons with age inbetween criteria can be accessed

--- LambdaDemo/Program.cs
+++ LambdaDemo/Program.cs
@@ -16,11 +16,24 @@ namespace LambdaDemo
 
             //UC 2: Accessing top 2 records whose Age below 60
             AccessRecordsAgeWise(personList, 2, 60);
+
+            //UC 3: Accessing records whose Age between 13-18
+            AccessRecordsForAges(personList, 13, 18);
+        }
+
+        private static void AccessRecordsForAges(List<Person> personList, int startAge, int endAge)
+        {
+            Console.WriteLine("persons with age between 13-18");
+            foreach(Person person in personList.FindAll(e => (e.age > startAge && e.age < endAge)))
+            {
+                Console.WriteLine("Name : " + person.name + "\t\tAge: " + person.age);
+            }
         }
 
         private static void AccessRecordsAgeWise(List<Person> personList,int records,int belowAge)
         {
-            foreach (Person person in personList.FindAll(e => (e.age < belowAge)).Take(records).ToList()) 
+            Console.WriteLine("Top 2 Persons with age below 60");
+            foreach (Person person in personList.FindAll(e => (e.age < belowAge)).Take(records).ToList())
             {
                 Console.WriteLine("Name : " + person.name + "\t\tAge: " + person.age);
             }
@@ -31,9 +44,20 @@ namespace LambdaDemo
 
             personList.Add(new Person(101, "tkr", "palas", 21));
 
+            personList.Add(new Person(101, "tkr", "palas", 58));
+
             personList.Add(new Person(102, "krt", "plasa", 65));
 
             personList.Add(new Person(103, "rkt", "pasa", 19));
+
+            personList.Add(new Person(104, "rikit", "pasa", 14));
+
+            personList.Add(new Person(105, "krit", "pasa", 10));
+
+            personList.Add(new Person(106, "rkit", "pasa", 15));
+
+            personList.Add(new Person(107, "rikita", "pasa", 16));
+
         }
     }
 }
